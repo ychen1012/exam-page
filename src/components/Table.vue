1cbb8cf2a1fdb5d90f6df7a26d790ccfe8bb3890
Yangchen <2015011446@mail.bistu.edu.cn>
2019-04-16 23:27:39 +0800

    学生页面管理 修改界面数据内容

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -4,31 +4,33 @@
       <el-col :span="24">
         <!--表单-->
         <el-form :inline="true" :model="formInline" class="demo-form-inline">
-          <el-form-item label="题目">
-            <el-input v-model="formInline.user.name" placeholder="姓名" style="width: 140px;"></el-input>
+          <el-form-item label="阶段">
+            <el-input v-model="formInline.question.name" placeholder="阶段" style="width: 140px;"></el-input>
           </el-form-item>
-          <el-form-item label="年份">
-            <el-date-picker
-              v-model="formInline.user.date"
-              align="right"
-              type="year"
-              placeholder="选择年份">
-            </el-date-picker>
-          </el-form-item>
-          <el-form-item label="地址">
-            <el-cascader expand-trigger="hover" :options="options" v-model="formInline.user.address"></el-cascader>
-          </el-form-item>
-          <el-form-item label="籍贯">
-            <el-select v-model="formInline.user.place" placeholder="请选择">
-              <el-option
-                v-for="item in places"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
-          </el-form-item>
+          <!--          <el-form-item label="年份">-->
+          <!--            <el-date-picker-->
+          <!--              v-model="formInline.user.date"-->
+          <!--              align="right"-->
+          <!--              type="year"-->
+          <!--              placeholder="选择年份">-->
+          <!--            </el-date-picker>-->
+          <!--          </el-form-item>-->
+          <!--          <el-form-item label="地址">-->
+          <!--            <el-cascader expand-trigger="hover" :options="options" v-model="formInline.user.address"></el-cascader>-->
+          <!--          </el-form-item>-->
+          <!--          <el-form-item label="籍贯">-->
+          <!--            <el-select v-model="formInline.user.place" placeholder="请选择">-->
+          <!--              <el-option-->
+          <!--                v-for="item in places"-->
+          <!--                :label="item.label"-->
+          <!--                :value="item.value">-->
+          <!--              </el-option>-->
+          <!--            </el-select>-->
+          <!--          </el-form-item>-->
           <el-button type="primary" @click="onSubmit">查询</el-button>
-          <a href="javascript:;" id="download" style="background-color:#409EFF;color: #fff;padding: 12px 10px!important;margin-left: 10px!important;border-radius:4px " @click="download()" download="download.csv">导出数据</a>
+          <a href="javascript:;" id="download"
+             style="background-color:#409EFF;color: #fff;padding: 12px 10px!important;margin-left: 10px!important;border-radius:4px "
+             @click="download()" download="download.csv">导出数据</a>
         </el-form>
         <!--表格-->
         <el-table
@@ -38,18 +40,23 @@
           <el-table-column type="selection">
           </el-table-column>
           <el-table-column
-            prop="date"
-            label="出生日期"
+            prop="id"
+            label="题号"
             width="180">
           </el-table-column>
           <el-table-column
             prop="name"
-            label="姓名"
+            label="题目"
             width="180">
           </el-table-column>
           <el-table-column
-            prop="address"
-            label="地址">
+            prop="jieduan"
+            label="阶段">
+          </el-table-column>
+
+          <el-table-column
+            prop="desc"
+            label="描述">
           </el-table-column>
           <el-table-column label="操作">
             <template slot-scope="scope">
@@ -70,16 +77,17 @@
         </div>
       </el-col>
     </el-row>
-    <el-dialog title="修改个人信息" :visible="dialogFormVisible" size="tiny">
+    <el-dialog title="修改题目" :visible="dialogFormVisible" size="tiny">
       <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="姓名">
+        <el-form-item label="题目">
           <el-input v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="地址">
-          <el-input v-model="form.address"></el-input>
+        <el-form-item label="阶段">
+          <el-input v-model="form.jieduan"></el-input>
         </el-form-item>
-        <el-form-item label="出生日期">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 100%;" ></el-date-picker>
+        <el-form-item label="描述">
+          <el-input type="textarea" :rows="8" placeholder="题目描述" v-model="form.desc"></el-input>
+<!--          <el-date-picker type="date" placeholder="选择日期" v-model="form.date" style="width: 100%;"></el-date-picker>-->
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="handleSave" :loading="editLoading">修改</el-button>
@@ -92,27 +100,29 @@
 <script type="text/ecmascript-6">
   const ERR_OK = "000";
   export default {
-    data () {
+    data() {
       return {
         formInline: {
-          user: {
-            name: '',
-            date: '',
-            address: [],
-            place: ''
+          question: {
+            id:'',//这是题目编号
+            name: '',//题目名称
+            desc:'',//题目描述
+            jieduan:'',//阶段
+
           }
         },
         tableData: [
-         {
-        date: '2016-05-02',
-        name: '王小虎',
-        province: '上海',
-        city: '普陀区',
-        address: '上海市普陀区金沙江路 1518 弄',
-        zip: 200333,
-        class:'软工1503',
-        number:'2015011446'
-      },
+          {
+
+            name: '两数之和',
+            id: 200333,
+            jieduan: '线性表',
+            desc: 'Given an array of integers, return indices of the two numbers such that they add up to a specific target.\n' +
+              '\n' +
+              'You may assume that each input would have exactly one solution, and you may not use the same element twice.'
+          },
+
+
         ],
         options: [],
         places: [],
@@ -127,7 +137,7 @@
         table_index: 999,
       };
     },
-    created () {
+    created() {
       this.$http.get('/api/getTable').then((response) => {
         response = response.data;
         if (response.code === ERR_OK) {
@@ -143,22 +153,22 @@
       });
     },
     methods: {
-      onSubmit () {
+      onSubmit() {
         this.$message('模拟数据，这个方法并不管用哦~');
       },
-      handleDelete (index, row) {
+      handleDelete(index, row) {
         this.tableData.splice(index, 1);
         this.$message({
           message: "操作成功！",
           type: 'success'
         });
       },
-      handleEdit (index, row) {
+      handleEdit(index, row) {
         this.dialogFormVisible = true;
         this.form = Object.assign({}, row);
         this.table_index = index;
       },
-      handleSave () {
+      handleSave() {
         this.$confirm('确认提交吗？', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
@@ -183,7 +193,7 @@
 
         });
       },
-      download: function() {
+      download: function () {
         console.log("xiazai")
         var obj = document.getElementById('download');
         var str = "姓名,出生日期,地址\n";
@@ -213,6 +223,7 @@
     text-align: center;
     margin-top: 30px;
   }
+
   .el-message-box__btns .cancel {
     float: right;
     margin-left: 10px;
--- src/components/student/examInfo.vue
+++ src/components/student/examInfo.vue
@@ -1,5 +1,6 @@
 <template>
     <h1>考试安排</h1>
+
 </template>
 
 <script>
